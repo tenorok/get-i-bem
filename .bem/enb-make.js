569537dd9f70ea150caff633600cd5964b7151bc
tenorok <mail@tenorok.ru>
2014-06-04 15:08:02 +0400

    Добавлена технология склеивания js-файлов без обрамляющих комментириев

--- .bem/enb-make.js
+++ .bem/enb-make.js
@@ -6,7 +6,7 @@ module.exports = function(config) {
             [ require('enb/techs/file-provider'), { target: '?.bemdecl.js' } ],
             require('enb/techs/deps-old'),
             require('enb/techs/files'),
-            require('enb/techs/js'),
+            require('../techs/js'),
             [ require('enb-borschik/techs/borschik'), { sourceTarget: '?.js', destTarget: '?.min.js', minify: true } ]
         ]);
 
